b9e3d08cd1d6b792bec156ca2c8a56974abe6230
Roy Reveltas <roy@codeandsend.com>
2016-08-11 21:41:53 +0100

    Added astral unicode characters recognition and tightened the tests

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+var from = require('array-from');
+
 module.exports = function (str, i, count, insert) {
-	return str.slice(0, i) + (insert || '') + str.slice(i + count);
+	var arr = from(str);
+	var processed = arr.slice(0, i);
+	if (insert && (insert.length > 0)) {
+		processed.push(insert);
+	}
+	processed.push(arr.slice(i + count).join(''));
+	var res = processed.join('');
+	return res;
 };
